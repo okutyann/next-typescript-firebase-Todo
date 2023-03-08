b4a495059a12ba62647386562c5ba1c6534c9108
okutyann <h19981029@gmail.com>
2023-03-08 23:12:15 +0900

    ✨ todolist

--- src/component/TodoList.tsx
+++ src/component/TodoList.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import styles from "@/styles/Home.module.css";
 
+type TypeTodo = {
+  text: string;
+  isCompleted: boolean;
+};
+
 export const TodoList = () => {
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<TypeTodo[]>([]);
   const [input, setInput] = useState<string>("");
   const add = (e: any) => {
     setInput(e.target.value);
@@ -17,14 +22,29 @@ export const TodoList = () => {
     // some
     // todosの中からinputと同じものがあればtrue
     // なければfalseを返す
-    if (todos.some((todo) => todo === input)) {
+    if (todos.some((todo) => todo.text === input)) {
       alert("既に追加されています。");
       setInput("");
       return;
     }
 
-    setTodos([...todos, input]);
+    const newTodo: TypeTodo = {
+      text: input,
+      isCompleted: false,
+    };
+    setTodos([...todos, newTodo]);
     setInput("");
+  };
+  const completed = (e: any, index: number) => {
+    // e.target.checked ? alert("完了") : alert("未完了");
+    console.log(e.target.checked, index);
+    const newTodo = todos.map((todo, i) => {
+      if (i === index) {
+        todo.isCompleted = e.target.checked;
+      }
+      return todo;
+    });
+    setTodos(newTodo);
   };
   return (
     <div className={styles.main}>
@@ -40,12 +60,39 @@ export const TodoList = () => {
           onChange={(e) => add(e)}
         />
         <ul className={styles.ul}>
-          {todos.map((todo, index) => (
-            <li key={index}>
-              <input type="checkbox" />
-              {todo}
-            </li>
-          ))}
+          {todos.length !== 0 &&
+            todos.map((todo, index) => (
+              <div key={todo.text}>
+                {!todo.isCompleted && (
+                  <li>
+                    <input
+                      type="checkbox"
+                      checked={todo.isCompleted}
+                      onChange={(e) => completed(e, index)}
+                    />
+                    {todo.text}
+                  </li>
+                )}
+              </div>
+            ))}
+        </ul>
+        <hr />
+        <ul className={styles.ul}>
+          {todos.length !== 0 &&
+            todos.map((todo, index) => (
+              <div key={todo.text}>
+                {todo.isCompleted && (
+                  <li>
+                    <input
+                      type="checkbox"
+                      checked={todo.isCompleted}
+                      onChange={(e) => completed(e, index)}
+                    />
+                    {todo.text}
+                  </li>
+                )}
+              </div>
+            ))}
         </ul>
       </form>
     </div>
